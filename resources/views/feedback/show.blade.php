e0e80e9811ba90728e0b2ccb3bed793a1c4ad576
develr57 <47880662+develr57@users.noreply.github.com>
2020-06-12 17:18:50 +0500

    2
    Форма свёрстана

--- resources/views/feedback/show.blade.php
+++ resources/views/feedback/show.blade.php
@@ -3,16 +3,40 @@
 @section('content')
 <div class="container-fluid">
     <h2 class="hn">Заполните форму</h2>
-    <form action="/feedback/store" method="post" id="fdform">
+    <form action="/feedback/store" method="post" id="fdform" class="fdform">
         @csrf
-        <label>Как к вам обращаться? </label>
-        <input type="text" name="name">
-        <label>E-mail: </label>
-        <input type="email" name="email">
-        <label>Телефон: </label>
-        <input type="text" name="phone">
-        <label>Комментарий: </label>
-        <textarea name="comment" form="fdform" rows="12"></textarea>
+        <div class="col-lg-9">
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Как к вам обращаться? </label>
+            <div class="col-md-9">
+                <input class="form-control" type="text" name="name">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">E-mail: </label>
+            <div class="col-md-9">
+                <input class="form-control" type="email" name="email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Телефон: </label>
+            <div class="col-md-9">
+                <input class="form-control" type="text" name="phone">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Комментарий: </label>
+            <div class="col-md-9">
+                <textarea class="form-control" name="comment" form="fdform" rows="8"></textarea>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-3 text-center">
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </div>
+        </div>
+        </div>
+        <div class="col-lg-3"></div>
     </form>
 </div>
 @endsection
