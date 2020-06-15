4fc90c4fb19174abde389bf4684bd0a7b490fbbc
SysAdmin <C:\Users\Admin\AppData\Roaming\The Bat!>
2020-06-15 16:50:18 +0500

    Ничего особенного
    Рзабираюсь с ауентификацией

--- config/database.php
+++ config/database.php
@@ -54,6 +54,8 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
+            // 'charset' => 'utf8mb4',
+            // 'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/feedback/notaccepted', 'FeedbackController@notaccepted');
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
